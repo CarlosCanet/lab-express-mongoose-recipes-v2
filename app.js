d8cf7cb146661c89c4bdf59266fa5ef6d8d185b6
Carlos Canet <carlos@carlosca.net>
2025-09-03 15:35:26 +0200

    Lab done. Now let's cook!

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require("express");
 const logger = require("morgan");
+const mongoose = require("mongoose");
+const Recipe = require("./models/Recipe.model");
 
 const app = express();
 
@@ -11,7 +13,11 @@ app.use(express.json());
 
 // Iteration 1 - Connect to MongoDB
 // DATABASE CONNECTION
-
+const MONGODB_URI = "mongodb://127.0.0.1:27017/express-mongoose-recipes-dev"
+mongoose
+  .connect(MONGODB_URI)
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
 
 
 // ROUTES
@@ -23,23 +29,63 @@ app.get('/', (req, res) => {
 
 //  Iteration 3 - Create a Recipe route
 //  POST  /recipes route
+app.post('/recipes', async (req, res) => {
+    try {
+        await Recipe.create({ ...req.body });
+        res.status(201).send("Recipe successfully created!");
+    } catch (error) {
+        res.status(500).send("Something went wrong creating a recipe");
+    }
+});
 
 
 //  Iteration 4 - Get All Recipes
 //  GET  /recipes route
+app.get('/recipes', async (req, res) => {
+    try {
+        const response = await Recipe.find();
+        res.json(response);
+    } catch (error) {
+        res.status(500).send("Something went wrong getting the recipe list");
+    }
+});
 
 
 //  Iteration 5 - Get a Single Recipe
 //  GET  /recipes/:id route
-
+app.get('/recipes/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const response = await Recipe.findById(id);
+        res.json(response);
+    } catch (error) {
+        res.status(500).send("Something went wrong getting the recipe");
+    }
+});
 
 //  Iteration 6 - Update a Single Recipe
 //  PUT  /recipes/:id route
-
+app.put('/recipes/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Recipe.findByIdAndUpdate(id, { ...req.body });
+        res.send("Recipe updated");
+    } catch (error) {
+        res.status(500).send("Something went wrong updating the recipe");
+    }
+});
 
 //  Iteration 7 - Delete a Single Recipe
 //  DELETE  /recipes/:id route
-
+app.delete('/recipes/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Recipe.findByIdAndDelete(id)
+        res.status(204).send("Recipe deleted");
+    } catch (error) {
+        res.status(500).send("Something went wrong deleting the recipe");
+    }
+});
 
 
 // Start the server
